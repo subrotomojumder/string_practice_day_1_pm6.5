09d4cec4a71b8aeebcec1af3ec9757bd7bedfc9e
subrotomojumder <subrotomojumder03@gmail.com>
2026-01-12 22:25:48 +0600

    solved balloon count problem

--- B_ICPC_Balloons.cpp
+++ B_ICPC_Balloons.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        int balloons = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i != 0 && s[i] == s[i - 1])
+            {
+                balloons += 1;
+            }
+            else
+            {
+                balloons += 2;
+            }
+        }
+        cout << balloons << endl;
+    }
     return 0;
 }
